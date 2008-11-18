1fb5426129115e4c2947dedd5352ab297273ebeb
daniel.baluta <daniel.baluta@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-11-18 20:38:09 +0000

    Added init_ash_fs and exit_ash_fs skeletons.

--- trunk/src/ash/super.c
+++ trunk/src/ash/super.c
@@ -8,4 +8,30 @@
  * For licensing information, see the file 'LICENCE'
  */
 
+
+static struct file_system_type ash_fs_type = {
+	.owner 		= THIS_MODULE,
+	.name  		= "ash",
+	.get_sb 	= ash_get_sb,
+	.kill_sb	= ash_kill_sb,
+};
+
+
+static int __init init_ash_fs(void)
+{
+
+	return 0;
+}
+
+static void __exit exit_ash_fs(void)
+{
+
+}
+
+
+module_init(init_ash_fs);
+module_exit(exit_ash_fs);
 	
+MODULE_DESCRIPTION("Ash File System");
+MODULE_AUTHOR("DB.GS");
+MODULE_LICENSE("MIT");
