b9e3b4a04f3e1cf5fc1f8a611a9eb3c8a6ca85ac
daniel.baluta <daniel.baluta@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-11-18 20:27:07 +0000

    Adding header information to files in /src/ash

---
+++ src/ash/read.c
@@ -0,0 +1,11 @@
+/*
+ * Ash File System
+ *
+ * Created by:
+ * 			   Daniel Baluta  <daniel.baluta@gmail.com> 
+ * 			   Gabriel Sandu  <gabrim.san@gmail.com>
+ *
+ * For licensing information, see the file 'LICENCE'
+ */
+
+	
