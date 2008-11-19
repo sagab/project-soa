2afb6b46db4ffc02fe72cc68e78798828a8529c1
daniel.baluta <daniel.baluta@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-11-19 07:23:23 +0000

    directory operations

---
+++ trunk/src/ash/dir.c
@@ -0,0 +1,30 @@
+/*
+ * Ash File System
+ *
+ * Created by:
+ * 			   Daniel Baluta  <daniel.baluta@gmail.com> 
+ * 			   Gabriel Sandu  <gabrim.san@gmail.com>
+ *
+ * For licensing information, see the file 'LICENCE'
+ */
+
+
+static int ash_fs_link (struct dentry *, struct inode *, struct dentry *);
+
+
+
+struct file_operations ash_dir_operations{
+	.dir 		=	TODO; 
+	.readdir	=	TODO;
+};
+
+struct inode_operations ash_dir_inode_operations{
+	.create		= ash_fs_create,
+	.lookup		= ash_fs_lookup,
+	.link		= ash_fs_link,
+	.unlink		= ash_fs_unlink,
+	//...
+};
+
+
+	
