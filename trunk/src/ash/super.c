780bcbf1667fe26dec68f1732c94179ce45f9866
gabrim.san <gabrim.san@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-11-29 11:32:26 +0000

    - some changes to README - making the skeleton for super.c

--- trunk/src/ash/super.c
+++ trunk/src/ash/super.c
@@ -13,41 +13,60 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 
-static struct super_operations ash_super_operations = {
-	//.alloc_inode = ash_alloc_inode,
-	//more operations to come here
-};
+#define ASH_MAGIC	0x123456
+
+
+static int ash_fill_super(struct super_block *sb, void *data, int silent)
+{
+	static struct tree_descr files[] = {{""}};
+	return simple_fill_super(sb, ASH_MAGIC, files);
+}
 
 static int ash_get_sb(struct file_system_type *fs,
 		int flags, const char *dev_name,
 		void *data, struct vfsmount *mnt)
 {
-	return 0;
+	return get_sb_bdev(fs, flags, dev_name, data, ash_fill_super, mnt);
 }
 
 
 static void ash_kill_sb(struct super_block *sb)
 {
-
+	kill_block_super(sb);
 }
 
 
 static struct file_system_type ash_fs_type = {
-	.owner 		= THIS_MODULE,
-	.name  		= "ash",
+	.owner		= THIS_MODULE,
+	.name 		= "ash",
+	.fs_flags	= FS_REQUIRES_DEV,
 	.get_sb 	= ash_get_sb,
 	.kill_sb	= ash_kill_sb,
 };
 
+static struct super_operations ash_super_operations = {
+	/*
+	.alloc_inode		= ash_alloc_inode,
+	.destroy_inode	= ash_destroy_inode,
+	.dirty_inode		= ash_dirty_inode,
+	.write_inode		= ash_write_inode,
+	.delete_inode		= ash_delete_inode,
+	.put_super		= ash_put_super,
+	.write_super		= ash_write_super,
+	.sync_fs		= ash_sync_fs,
+	.statfs		= ash_statfs,
+	.remount_fs		= ash_remount_fs,
+	*/
+};
 
 static int __init init_ash_fs(void)
 {
-	return 0;
+	return register_filesystem(&ash_fs_type);
 }
 
 static void __exit exit_ash_fs(void)
 {
-
+	unregister_filesystem(&ash_fs_type);
 }
 
 module_init(init_ash_fs);
