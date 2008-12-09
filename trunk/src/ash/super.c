f02a819898364e121ee1a691fadf3584c5da0d5d
gabrim.san <gabrim.san@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-12-09 16:35:01 +0000

    - ash_fill_super is working! :D - TODO for me: read the root dir permissions from the root entry -> will take care of it later - TODO general: start writing more operations

--- trunk/src/ash/super.c
+++ trunk/src/ash/super.c
@@ -40,36 +40,69 @@ static int ash_fill_super(struct super_block *sb, void *data, int silent)
 	struct inode * root;
 	struct dentry * root_dentry;
 	struct buffer_head * bh;
+	struct ash_raw_superblock *rsb;
+	mode_t def;
 
-	// init the superblock fields
-	sb->s_magic = ASH_MAGIC;
-	sb->s_blocksize = 4096;
-	sb->s_op = &ash_super_operations;
-
-	printk("phase 1 mounted \n");
-	
 	// read sector 0 -> the superblock sector
 	bh = __bread(sb->s_bdev, 0, ASH_SECTORSIZE);
+	rsb = (struct ash_raw_superblock*)bh->b_data;
+	
+	// check if it's a ash filesystem
+	if (rsb->magic != ASH_MAGIC) {
+		printk(KERN_ERR "incorrect magic number\n");
+		brelse(bh);
+		return -1;
+	}
+	
+	// fill in superblock fields by using the superblock read from disk
+	sb->s_blocksize = rsb->blocksize;
+	sb->s_blocksize_bits = rsb->blockbits;
+	sb->s_magic = rsb->magic;
+	
+	// setting time granularity at 1 second (it is in ns)
+	sb->s_time_gran = 1000000000;
+	
+	// private filesystem info
+	sb->s_fs_info = rsb;	// now, I wonder what happens to rsb after I brelse(bh) ^^.
+	
+	
+	// sanity check
+	printk("vers: %d volname: '%s'\n", rsb->vers, rsb->volname);
+
 
 	// create the root inode
-	root = ash_make_inode(sb, S_IFDIR | 0755);
-	if (! root)
+	
+	// normal file permissions: owner can read/write/execute, the group and
+	// others can only r/w
+	def = S_IRWXU | (S_IRGRP|S_IWGRP) | (S_IROTH|S_IWOTH);
+	
+	// root is also a directory, so S_IFDIR
+	root = ash_make_inode(sb, S_IFDIR | def);
+	if (! root) {
+		brelse(bh);
 		return -ENOMEM;
+	}
 
 	root->i_op = &simple_dir_inode_operations;
 	root->i_fop = &simple_dir_operations;
 
 	root_dentry = d_alloc_root(root);
 	if (! root_dentry) {
+		brelse(bh);
 		iput(root);
 		return -ENOMEM;
 	}
-	
-	sb->s_root = root_dentry;
 
-	printk("all done: %d", bh->b_size);
+	// final superblock init
+	sb->s_root = root_dentry;
+	sb->s_op = &ash_super_operations;
 
 	brelse(bh);
+	
+	if (rsb != NULL)
+		printk("omg, a ramas si %d\n", rsb->vers);
+	else
+		printk("oops.\n");
 
 	return 0;
 }
@@ -89,8 +122,8 @@ static void ash_kill_sb(struct super_block *sb)
 
 
 static struct file_system_type ash_fs_type = {
-	.owner	= THIS_MODULE,
-	.name 	= "ash",
+	.owner		= THIS_MODULE,
+	.name 		= "ash",
 	.get_sb 	= ash_get_sb,
 	.kill_sb	= ash_kill_sb,
 };
