82225b1124f36d37b012834845730e29483966ac
gabrim.san <gabrim.san@5f00c06c-97bb-11dd-8e58-870c635edf7a>
2008-12-10 21:23:05 +0000

    - rewritten ashformat.c but all the computations are messed up >.< - TODO. Fix the bugs in >> on uint64's. WTF.

--- src/tools/ashformat.c
+++ src/tools/ashformat.c
@@ -13,15 +13,16 @@
 #include <string.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <math.h>
 
 #include "ash.h"
 
 
 /**
- * Get the size in sectors of a device
+ * Get the size in bytes of a device
  *
  * @device name of the device in the /dev system
- * @return the size as a 64bit unsigned integer value. -1 on error.
+ * @return the size of the device in bytes. 0 on error.
  */
 uint64_t getsize(char *device)
 {
@@ -32,6 +33,12 @@ uint64_t getsize(char *device)
 	// we need to build path to point to this file: /sys/block/sdb/sdb1/size
 	strcpy(path, "/sys/block/");
 	char *devpos = strrchr(device,'/');
+	
+	if (! devpos || strlen(devpos) < 5) {
+		printf("'%s' is not a device path\n", device);
+		return 0;
+	}
+	
 	devpos++;
 	
 	strncat(path, devpos, 3);
@@ -44,7 +51,7 @@ uint64_t getsize(char *device)
 	FILE *f = fopen(path, "r");
 	if (!f) {
 		printf("cannot open '%s'\n", path);
-		return -1;
+		return 0;
 	}
 	
 	uint64_t rez;
@@ -55,8 +62,11 @@ uint64_t getsize(char *device)
 	
 	if (sw != 1) {
 		printf("unknown file format for '%s'\n", path);
-		return -1;
+		return 0;
 	}
+	
+	// multiply the size in sectors by sectorsize (512 = 2^9)
+	rez <<= ASH_SECTORBITS;
 	
 	return rez;
 }
@@ -64,12 +74,14 @@ uint64_t getsize(char *device)
 
 
 /**
- * Format the device with AshFS and the size in sectors
+ * Format the device with AshFS
  * @device The name of the device in /dev
- * @size Size given as the number of sectors
+ * @bsize block size in bytes for Ash
+ * @size device capacity in bytes
+ * @name volume name
  * @return 0 on ok.
  */
-int format(char *device, uint64_t size)
+int format(char *device, uint16_t bsize, uint64_t size, char *volname)
 {
 	// fill in a superblock structure
 	struct ash_raw_superblock s;
@@ -78,18 +90,23 @@ int format(char *device, uint64_t size)
 	memset(&s, 0, sizeof(s));
 	
 	s.sectorsize = ASH_SECTORSIZE;
-	s.blocksize = ASH_BLOCKSIZE;
-	s.blockbits = ASH_BLOCKBITS;
-	s.maxsectors = size;
+	s.blocksize = bsize;
+	s.blockbits = (uint16_t) (ceil(log(bsize)/log(2)));
 	
-	// slightly different formula than on the wiki
-	// http://code.google.com/p/project-soa/wiki/PhysicalStructureeinstein
-	uint32_t tmp = (uint32_t)s.blocksize + (uint32_t)4;
-	s.maxblocks = (uint32_t)( size * s.sectorsize - 2 * s.sectorsize ) / tmp;
+	// formula on the wiki
+	// http://code.google.com/p/project-soa/wiki/PhysicalStructure
+	long long mb = (long long)size / 4096;
+	printf("size %d, max block %d\n ", size, mb);
+	s.maxblocks = size >> s.blockbits;
+	s.UBBsize = s.maxblocks >> 3;
+	s.BATsize = s.maxblocks << 2;
 	
-	s.BATsize = s.maxblocks * 4;
-	s.BATsectors = s.BATsize / s.sectorsize + 1;
-	s.datastart = s.rootentry = 1 + s.BATsectors;
+	uint16_t total = sizeof(s) + s.UBBsize + s.BATsize;
+	s.datastart = total >> s.blockbits;
+	
+	// in case it occupies more, take the additional space up to next block
+	if (total > (s.datastart << s.blockbits))
+		s.datastart ++;
 	
 	s.mnt_count = 0;
 	s.max_mnt_count = ASH_MAX_MOUNTS;
@@ -101,11 +118,11 @@ int format(char *device, uint64_t size)
 	s.last_check = now;
 	s.max_check_time = ASH_MAX_CHECK;
 	
-	s.state = ASH_FAST_FORMAT;
+	s.state = ASH_UMOUNT;
 	s.magic = ASH_MAGIC;
 	s.vers = ASH_VERSION;
 	
-	strcpy(s.volname, "usbstick");
+	strcpy(s.volname, volname);
 	
 	// filling in the root directory entry
 	struct ash_raw_file rentry;
@@ -115,14 +132,15 @@ int format(char *device, uint64_t size)
 	
 	// sets bits for directory, r/w/x for owner, r/w group & others
 	rentry.mode = S_IFDIR | S_IRWXU | (S_IRGRP | S_IXGRP) | (S_IROTH | S_IXOTH);
+	rentry.ashtype = ASHTYPE_NORMAL;
 	rentry.uid = rentry.gid = 1;
 	rentry.size = 0;
 	rentry.atime = rentry.wtime = rentry.ctime = now;
-	rentry.startblock = s.rootentry;
+	rentry.startblock = s.datastart;
 	rentry.namelength = 0;		// root dir doesn't have a name
 	
 	// trying to open the device file
-	FILE *fd = fopen(device, "w");
+	FILE *fd = fopen("/root/test.log", "w+");
 	if (!fd) {
 		printf("cannot open device for writing\n");
 		return 1;	
@@ -135,8 +153,84 @@ int format(char *device, uint64_t size)
 		return 1;
 	}
 	
+	// writing the used block bitmap
+	uint8_t buf[512];
+	
+	// bytes representing used blocks -> therefore = FF
+	uint16_t bytes = s.datastart >> 3;
+	
+	// number of unused blocks in the last byte
+	uint16_t bits = 8 - s.UBBsize & 7;
+	
+	// this will insert zeroes in the octet
+	uint16_t last = (0xFF >> bits) << bits;
+	
+	// unless we're somehow talking about a 22 TB flash drive,
+	// there's no way for bytes >= 512, so they all fit in buf.
+	memset(buf, 0, 512);		// clear all
+	memset(buf, 0xFF, bytes);	// set bytes of them to 0xFF
+	buf[bytes] = last;		// set the last partially used one
+	
+	printf("UBB used bytes: %d, bits: %d, last= %x\n", bytes, bits, last);
+	
+	// write first bytes + 1 bytes.
+	sw = fwrite(&buf, sizeof(uint8_t), bytes + 1, fd);
+	if (sw != bytes + 1) {
+		printf("error while writing UBB\n");
+		return 1;
+	}
+	
+	// write last part of big 0-es
+	uint16_t left = s.UBBsize - bytes - 1;		// bytes left to copy
+	int n = left >> 9;		// chunks of 512 bytes from left
+	memset(buf, 0, 512);
+	
+	// mass copy
+	int i;
+	for (i = 0; i < n; i++) {
+		sw = fwrite(&buf, sizeof(uint8_t), 512, fd);
+		if (sw != 512) {
+			printf("error while writing UBB\n");
+			return 1;
+		}
+		
+		left -= 512;	// adjust number of bytes left
+	}
+	
+	// small size copy
+	sw = fwrite(&buf, sizeof(uint8_t), left, fd);
+	if (sw != left) {
+		printf("error while writing UBB\n");
+		return 1;
+	}
+
+	printf("UBB left=%d n=%d\n", left, n);
+	
+	// writing BAT table with zeroes
+	memset(buf, 0, 512);
+
+	left = s.BATsize;
+	n = left >> 9;
+	for (i = 0; i < n; i++) {
+		sw = fwrite(&buf, sizeof(uint8_t), 512, fd);
+		if (sw != 512) {
+			printf("error while writing BAT\n");
+			return 1;
+		}
+		
+		left -= 512;
+	}
+	
+	sw = fwrite(&buf, sizeof(uint8_t), left, fd);
+	if (sw != left) {
+		printf("error while writing UBB\n");
+		return 1;
+	}
+	
+	printf("BAT left=%d n=%d\n", left, n);
+	
 	// seek to the root directory entry location
-	if (fseek(fd, s.datastart * s.sectorsize, SEEK_SET) != 0) {
+	if (fseek(fd, rentry.startblock * s.blocksize, SEEK_SET) != 0) {
 		printf("error while trying to seek on the device file\n");
 		return 1;
 	}
@@ -154,16 +248,32 @@ int format(char *device, uint64_t size)
 	// printout verbose info
 	printf("\n");
 	printf("volume name: '%s'\n", s.volname);
-	printf("formatted with Ash vers. %4.2f\n", (float)s.vers/100);
-	printf("sector size: %d bytes\n", s.sectorsize);
+	printf("formatted with Ash vers. %4.2f\n", (float)s.vers/128);
 	printf("block size: %d bytes\n", s.blocksize);
-	printf("max sectors: %d\n", s.maxsectors);
+	printf("block bits: %d\n", s.blockbits);
 	printf("max blocks: %d\n", s.maxblocks);
+	printf("UBBsize: %d bytes\n", s.UBBsize);
 	printf("BATsize: %d bytes\n", s.BATsize);
-	printf("BATsectors: %d\n", s.BATsectors);
-	printf("start of data @ sector: %d\n\n", s.rootentry);
+	printf("datastart @ block: %d\n\n", s.datastart);
 	
 	return 0;
+}
+
+
+
+
+/*
+ * Prints instructions
+ *
+ */
+void instructions()
+{
+	printf("\n\tAshFS Disk Format Utility\n\n");
+	printf("usage:\t");
+	printf("./ashformat <dev> [-b <bsize>] [-n <volname>]\n");
+	printf("<dev>: name of the device to format (ex: /dev/sdb1)\n\n");
+	printf("<bsize>: size of logical block. Must be a multiple of 512. Default 4096\n");
+	printf("<volname>: 15 alfanum for name. Default 'usbstick'\n\n");
 }
 
 
@@ -176,24 +286,99 @@ int format(char *device, uint64_t size)
  */
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		printf("\n\tAshFS Disk Format Utility\n\n");
-		printf("usage:\t");
-		printf("./ashformat <dev>\n");
-		printf("<dev>: name of the device to format (ex: /dev/sdb1)\n\n");
+	if (argc < 2) {
+		instructions();
 		return 1;
 	}
 	
-	// get device info
-	uint64_t sectors = getsize(argv[1]);
-	if (sectors < 0) {
-		printf("the device '%s' isn't a valid block device!\n", argv[1]);
-		return 2;
+	char volname[16];
+	uint64_t size;
+	int bsize;
+	int devicearg;
+
+	// do some inits
+	devicearg = -1;
+	strcpy(volname, "usbstick");
+	bsize = ASH_BLOCKSIZE;
+
+	// start parsing the parameters
+	int p = 1;
+	
+	while(p<argc) {
+		if (strcmp(argv[p],"-n") == 0) {
+			
+			if (p+1 >= argc) {
+				printf("you need to specify a string if you use -n flag\n");
+				return 1;			
+			}
+			
+			int len = strlen(argv[p+1]);
+			
+			// test
+			if (len < 16) {
+			
+				// check each letter
+				int l, test = 1;
+				
+				for (l = 0; l < len; l++)
+					if (! isalnum(argv[p+1][l]) )
+						test = 0;
+						
+				if (!test) {
+					printf("volname contains illegal chars\n");
+					return 1;
+				}
+				
+				// all ok, copy name
+				strcpy(volname, argv[p+1]);
+				p+=2;
+			
+			} else {
+				printf("volname bigger than 15 letters\n");
+				return 1;
+			}
+			
+		} else
+			if (strcmp(argv[p],"-b") == 0) {
+				if (p+1 >= argc) {
+					printf("you are missing the blocksize parameter\n");
+					return 1;
+				}
+			
+				int r = sscanf(argv[p+1], "%d", &bsize);
+				
+				if (bsize < 512 || bsize > 8096) {
+					printf("blocksize must be between 512 and 8096.\n");
+					return 1;
+				}
+				
+				if (r == 0 || (bsize != bsize >> 9 << 9)) {
+					printf("blocksize is not a valid multiple of 512.\n");
+					return 1;
+				}
+				
+				p+=2;
+		
+			} else
+				devicearg = p++;
+	
+	}
+	
+	if (devicearg < 1) {
+		printf("you must specify a device to format\n");
+		return 1;
 	}
 	
 	
+	// get device info
+	size = getsize(argv[devicearg]);
+	if (size == 0) {
+		printf("the device '%s' isn't a valid block device!\n", argv[devicearg]);
+		return 2;
+	}
+	
 	// format the device media
-	int sw = format(argv[1], sectors);
+	int sw = format(argv[devicearg], bsize, size, volname);
 	if (sw == 0) {
 		printf("Formatting OK.\n");
 	} else
